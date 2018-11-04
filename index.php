34ccd7aacba6b2ccef0ff9a05cdfc3d5b1d833c2
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-05 11:50:06 +1300

    Setup default index layout

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php get_header(); ?>
 		<div class="container mt-2">
-			<?php if ( have_posts() ): ?>
-				<?php while ( have_posts() ): the_post(); ?>
-					<div class="card text-light bg-secondary mt-2">
-						<div class="card-header"><?php the_title(); ?></div>
-						<div class="card-body row">
-							<?php if ( has_post_thumbnail() ): ?>
-								<?php $colClass = 'col-md-8' ?>
+			<div class="row">
+				<div class="col">
+					<h1><?php bloginfo( 'name' ); wp_title( '-', true, 'left'); ?></h1>
+				</div>
+			</div>
 
-								<div class="col-md-4">
-									<?php the_post_thumbnail( 'medium', ['class' => 'img-fluid'] ); ?>
+			<div class="row">
+				<div class="col">
+					<?php if ( have_posts() ): ?>
+						<?php while ( have_posts() ): the_post(); ?>
+							<div class="row">
+								<?php if ( has_post_thumbnail() ): ?>
+									<div class="col-4 pr-0">
+										<?php the_post_thumbnail( 'medium', ['class' => 'img-fluid img-thumbnail'] ); ?>
+									</div>
+								<?php endif; ?>
+
+								<div class="col pl-0">
+									<?php the_content(); ?>
 								</div>
-							<?php else: ?>
-								<?php $colClass = 'col' ?>
-							<?php endif; ?>
-
-							<div class="<?php echo $colClass ?>">
-								<div class="card-text"><?php the_content(); ?></div>
 							</div>
-						</div>
-					</div>
-				<?php endwhile; ?>
-			<?php endif; ?>
+						<?php endwhile; ?>
+					<?php endif; ?>
+				</div>
+			</div>
 		</div>
 <?php get_footer(); ?>
