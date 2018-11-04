fe46d8e5463a6bdb87211335469308257af83ffd
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-05 11:30:53 +1300

    Setup navigation

--- functions.php
+++ functions.php
@@ -1,16 +1,20 @@
 <?php
 	/* Table of Contents:
-		1.0 Theme Actions
-			1.1 Stylesheets and Scripts
-			1.2 Menus
-			1.3 Sidebars
-		2.0 Theme Support
+		1.0 Plugins
+		2.0 Theme Actions
+			2.1 Stylesheets and Scripts
+			2.2 Menus
+			2.3 Sidebars
+		3.0 Theme Support
 	*/
 
 
-	// 1.0 Theme Actions
+	// 1.0 Plugins
+	require_once get_template_directory().'/class-wp-bootstrap-navwalker.php';
 
-	// 1.1 Stylesheets and Scripts
+	// 2.0 Theme Actions
+
+	// 2.1 Stylesheets and Scripts
 	add_action( 'wp_enqueue_scripts', function() {
 		$css_directory = get_template_directory_uri().'/assets/css/';
 		$js_directory = get_template_directory_uri().'/assets/js/';
@@ -23,13 +27,13 @@
 		wp_enqueue_script( 'bootstrapjs', $js_directory.'bootstrap.min.js', array(), '4.1.3', true );
 	});
 
-	// 1.2 Menus
+	// 2.2 Menus
 	add_action( 'init', function() {
 		// Default navigation menu
 		register_nav_menu( 'defaultnav', __( 'Default Navigation' ) );
 	});
 
-	// 1.3 Sidebars
+	// 2.3 Sidebars
 	add_action( 'widgets_init', function() {
 		register_sidebar(array(
 			'id'            => 'sidebar-main',
@@ -43,5 +47,5 @@
 	});
 
 
-	// 2.0 Theme Support
+	// 3.0 Theme Support
 	add_theme_support( 'post-thumbnails' );
