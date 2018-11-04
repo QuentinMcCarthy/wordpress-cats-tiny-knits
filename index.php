ac735255860f3d79c847f7b82eb9a19f48e0c2e7
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-05 11:33:19 +1300

    Separated footer and header

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-
-		<title><?php wp_title( '-', true, 'right'); bloginfo( 'name' ); ?></title>
-
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-		<?php if ( has_nav_menu( 'defaultnav' ) ): ?>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#default-nav-container" aria-controls="default-nav-container" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<a class="navbar-brand" href="#">Navbar</a>
-
-				<?php
-					wp_nav_menu(array(
-						'theme_location'  => 'defaultnav',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'default-nav-container',
-						'menu_class'      => 'navbar-nav mr-auto',
-						'menu_id'         => 'default-nav-menu',
-						'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-						'walker'          => new WP_Bootstrap_Navwalker(),
-					));
-				?>
-			</nav>
-		<?php endif; ?>
-
+<?php get_header(); ?>
 		<div class="container mt-2">
 			<?php if ( have_posts() ): ?>
 				<?php while ( have_posts() ): the_post(); ?>
@@ -53,7 +23,4 @@
 				<?php endwhile; ?>
 			<?php endif; ?>
 		</div>
-
-		<?php wp_footer(); ?>
-	</body>
-</html>
+<?php get_footer(); ?>
