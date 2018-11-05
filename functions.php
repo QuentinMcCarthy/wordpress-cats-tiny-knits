7a3bf3d7515a38b3e6b5739b6eaf9102b083c31e
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-06 11:29:41 +1300

    Fix table of contents

--- functions.php
+++ functions.php
@@ -1,20 +1,17 @@
 <?php
 	/* Table of Contents:
-		1.0 Plugins
-		2.0 Theme Actions
-			2.1 Stylesheets and Scripts
-			2.2 Menus
-			2.3 Sidebars
-		3.0 Theme Support
+		1.0:- Plugins
+		2.0:- Stylesheets and Scripts
+		3.0:- Menus
+		4.0:- Sidebars
+		5.0:- Post Thumbnails
 	*/
 
 
-	// 1.0 Plugins
+	// 1.0:- Plugins
 	require_once get_template_directory().'/class-wp-bootstrap-navwalker.php';
 
-	// 2.0 Theme Actions
-
-	// 2.1 Stylesheets and Scripts
+	// 2.0:- Stylesheets and Scripts
 	add_action( 'wp_enqueue_scripts', function() {
 		$css_directory = get_template_directory_uri().'/assets/css/';
 		$js_directory = get_template_directory_uri().'/assets/js/';
@@ -28,13 +25,13 @@
 		wp_enqueue_script( 'bootstrapjs', $js_directory.'bootstrap.min.js', array(), '4.1.3', true );
 	});
 
-	// 2.2 Menus
+	// 3.0:- Menus
 	add_action( 'init', function() {
 		// Default navigation menu
 		register_nav_menu( 'defaultnav', __( 'Default Navigation' ) );
 	});
 
-	// 2.3 Sidebars
+	// 4.0:- Sidebars
 	add_action( 'widgets_init', function() {
 		register_sidebar(array(
 			'id'            => 'sidebar-main',
@@ -48,5 +45,5 @@
 	});
 
 
-	// 3.0 Theme Support
+	// 5.0:- Theme Support
 	add_theme_support( 'post-thumbnails' );
