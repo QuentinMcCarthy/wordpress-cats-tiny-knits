903e4a05df4cd01ad50d83c247e5aa0a9175a287
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-06 10:37:03 +1300

    Moved container div out of index

--- header.php
+++ header.php
@@ -8,23 +8,24 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<?php if ( has_nav_menu( 'defaultnav' ) ): ?>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#default-nav-container" aria-controls="default-nav-container" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<a class="navbar-brand" href="#">Navbar</a>
+		<div class="container mt-2">
+			<?php if ( has_nav_menu( 'defaultnav' ) ): ?>
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#default-nav-container" aria-controls="default-nav-container" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<a class="navbar-brand" href="#">Navbar</a>
 
-				<?php
-					wp_nav_menu(array(
-						'theme_location'  => 'defaultnav',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'default-nav-container',
-						'menu_class'      => 'navbar-nav mr-auto',
-						'menu_id'         => 'default-nav-menu',
-						'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-						'walker'          => new WP_Bootstrap_Navwalker(),
-					));
-				?>
-			</nav>
-		<?php endif; ?>
+					<?php
+						wp_nav_menu(array(
+							'theme_location'  => 'defaultnav',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'default-nav-container',
+							'menu_class'      => 'navbar-nav mr-auto',
+							'menu_id'         => 'default-nav-menu',
+							'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+							'walker'          => new WP_Bootstrap_Navwalker(),
+						));
+					?>
+				</nav>
+			<?php endif; ?>
--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <?php get_header(); ?>
-		<div class="container mt-2">
-			<div class="row">
-				<div class="col">
-					<h1><?php bloginfo( 'name' ); wp_title( '-', true, 'left'); ?></h1>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col">
-					<?php if ( have_posts() ): ?>
-						<?php while ( have_posts() ): the_post(); ?>
-							<div class="row">
-								<?php if ( has_post_thumbnail() ): ?>
-									<div class="col-4 pr-0">
-										<?php the_post_thumbnail( 'medium', ['class' => 'img-fluid img-thumbnail'] ); ?>
-									</div>
-								<?php endif; ?>
-
-								<div class="col pl-0">
-									<?php the_content(); ?>
-								</div>
-							</div>
-						<?php endwhile; ?>
-					<?php endif; ?>
-				</div>
-			</div>
+	<div class="row">
+		<div class="col">
+			<h1><?php bloginfo( 'name' ); wp_title( '-', true, 'left'); ?></h1>
 		</div>
+	</div>
+
+	<div class="row">
+		<div class="col">
+			<?php if ( have_posts() ): ?>
+				<?php while ( have_posts() ): the_post(); ?>
+					<div class="row">
+						<?php if ( has_post_thumbnail() ): ?>
+							<div class="col-4 pr-0">
+								<?php the_post_thumbnail( 'medium', ['class' => 'img-fluid img-thumbnail'] ); ?>
+							</div>
+						<?php endif; ?>
+
+						<div class="col pl-0">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
 <?php get_footer(); ?>
